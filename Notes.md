8aa7ef7516f3610b007654526a110f529abcaa57
Mohamed Rafi <rafi.huta@gmail.com>
2021-07-06 13:46:55 +0530

    first.sh added

---
+++ Notes.md
@@ -0,0 +1,5 @@
+# Set up SSH keys on CENTOS
+
+# Add keys to Putty 
+
+# Connect CentOS using Remnote IP and ssh keys
---
+++ first.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+echo "List current directory"
+ls -ltar .
+
+echo "Änother way to run a bash script is ./first.sh"
+
+
